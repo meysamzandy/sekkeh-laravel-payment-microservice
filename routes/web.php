6111ef76b9683d08487506098f6a1e466136d191
ayeneh <it@ayeneh.co>
2021-01-27 16:12:26 +0330

    health check api added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health1', function () {
+    return response("i am alive",200);
+});
+
+Route::get('/health2', function () {
+    try{
+        \Illuminate\Support\Facades\DB::connection()->statement("show tables");
+        return response("i am ok",200);
+    }catch (Exception $e){
+        return response("i am not ok",500);
+    }
+});
