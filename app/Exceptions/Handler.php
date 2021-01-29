feeba4e72893b9b2e2328ef379a58eec6111077a
ayeneh <it@ayeneh.co>
2021-01-29 16:09:23 +0330

    sentry added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -25,6 +25,15 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function report(\Throwable $exception)
+    {
+        if ($this->shouldReport($exception) && app()->bound('sentry')) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      *
